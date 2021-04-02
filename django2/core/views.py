d4b14febe9d59ac3d6b03976534e00168755f0bf
kaique Bellmont <kaiquebellmont@gmail.com>
2021-04-02 13:58:09 -0300

    iniciando um novo projeto

---
+++ django2/core/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def contato(request):
+    return render(request, 'index.html')
+
+
+def produto(request):
+    return render(request, 'produto.html')
+
